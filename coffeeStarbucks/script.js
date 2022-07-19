3aa8ae85c0504ade5f57deaa676f2fe16a4b88c0
Al Jannatul Maowa <maowarisa123@gmail.com>
2022-07-19 14:57:47 +1000

    coffee starbuck and music app

---
+++ coffeeStarbucks/script.js
@@ -0,0 +1,7 @@
+function imgSlider(img){
+    document.querySelector('.starbucks').src = img;
+}
+function changeBackColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
